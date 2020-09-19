f6c98f516b911dd4a524bcb7883e63f84ec80307
Fireman9 <54212619+Fireman9@users.noreply.github.com>
2020-09-20 02:36:14 +0300

    Added styles and note time creating

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const list = document.getElementById("list");
 const textArea = document.getElementById("textArea");
 
 let notesArray;
+
 if (localStorage.getItem('notesArray')) {
     notesArray = JSON.parse(localStorage.getItem('notesArray'));
-    for(i in notesArray){
+    for (i in notesArray) {
         let newLi = document.createElement('li');
         newLi.id = notesArray[i].id;
-        if(notesArray[i].text.length > 0) {
+        if (notesArray[i].text.length > 0) {
             newLi.innerHTML = notesArray[i].text.slice(0, 15);
             let temp = newLi.innerHTML.split('\n');
             if (temp.length > 1) {
@@ -17,10 +18,23 @@ if (localStorage.getItem('notesArray')) {
             } else {
                 newLi.innerHTML = temp[0] + "...";
             }
-        }
-        else{
+        } else {
             newLi.innerHTML = 'New note';
         }
+        let time = new Date(parseInt(newLi.id));
+        let year = String(time.getFullYear());
+        let month = String(time.getMonth() + 1);
+        if (month.length < 2) month = "0" + month;
+        let date = String(time.getDate());
+        if (date.length < 2) date = "0" + date;
+        let hours = String(time.getHours());
+        if (hours.length < 2) hours = "0" + hours;
+        let minutes = String(time.getMinutes());
+        if (minutes.length < 2) minutes = "0" + minutes;
+        let seconds = String(time.getSeconds());
+        if (seconds.length < 2) seconds = "0" + seconds;
+        newLi.innerHTML += "<br>" + year + "." + month + "." +
+            date + " " + hours + ":" + minutes + ":" + seconds;
         list.append(newLi);
     }
 } else {
@@ -31,26 +45,40 @@ window.addEventListener("beforeunload", function (event) {
     localStorage.setItem('notesArray', JSON.stringify(notesArray));
 })
 
-addBut.onclick = function() {
+addBut.onclick = function () {
     let newLi = document.createElement('li');
     let date = new Date();
     newLi.id = String(date.getTime());
     let note = {id: newLi.id, text: ""};
     notesArray.push(note);
     newLi.innerHTML = 'New note';
+    let time = new Date(parseInt(newLi.id));
+    let year = String(time.getFullYear());
+    let month = String(time.getMonth() + 1);
+    if (month.length < 2) month = "0" + month;
+    let day = String(time.getDate());
+    if (day.length < 2) day = "0" + day;
+    let hours = String(time.getHours());
+    if (hours.length < 2) hours = "0" + hours;
+    let minutes = String(time.getMinutes());
+    if (minutes.length < 2) minutes = "0" + minutes;
+    let seconds = String(time.getSeconds());
+    if (seconds.length < 2) seconds = "0" + seconds;
+    newLi.innerHTML += "<br>" + year + "." + month + "." +
+        day + " " + hours + ":" + minutes + ":" + seconds;
     list.append(newLi);
 }
 
 list.onclick = function (event) {
-    if(event.target.tagName != "LI") return;
+    if (event.target.tagName != "LI") return;
     let li = event.target;
-    for(let i in notesArray){
-        if(notesArray[i].id == li.id){
+    for (let i in notesArray) {
+        if (notesArray[i].id == li.id) {
             document.getElementById("textArea").value = notesArray[i].text;
         }
     }
     let selected = list.querySelectorAll('.selected');
-    for(let elem of selected){
+    for (let elem of selected) {
         elem.classList.remove('selected');
     }
     li.classList.add('selected');
@@ -60,18 +88,18 @@ list.onclick = function (event) {
 
 delBut.onclick = function () {
     let selected = list.querySelectorAll('.selected');
-    if (selected.length == 0){
+    if (selected.length == 0) {
         alert("Nothing selected");
         // TODO: alert change
     }
     textArea.value = "";
     textArea.readOnly = true;
-    for(let i in notesArray){
-        if(notesArray[i].id == selected[0].id){
+    for (let i in notesArray) {
+        if (notesArray[i].id == selected[0].id) {
             notesArray.splice(i, 1);
         }
     }
-    for(let elem of selected){
+    for (let elem of selected) {
         elem.parentNode.removeChild(elem);
     }
 }
@@ -85,10 +113,9 @@ textArea.oninput = function () {
     }
     selected[0].textContent = textArea.value.slice(0, 15);
     let temp = selected[0].textContent.split('\n');
-    if(temp.length > 1){
+    if (temp.length > 1) {
         selected[0].textContent = temp[0];
-    }
-    else{
+    } else {
         selected[0].textContent = temp[0] + "...";
     }
 }
