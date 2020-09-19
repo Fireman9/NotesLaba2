5a59e29a583b2487745e733e5d9966b062a0f33f
Fireman9 <54212619+Fireman9@users.noreply.github.com>
2020-09-19 16:36:57 +0300

    added selecting note

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const addBut = document.getElementById("addBut");
+const delBut = document.getElementById("delBut");
 const list = document.getElementById("list");
-
-let notesArray = new Array();
 
 function addNewNote() {
     let newLi = document.createElement('li');
@@ -12,3 +11,13 @@ function addNewNote() {
 }
 
 addBut.addEventListener("click", addNewNote);
+
+list.onclick = function (event){
+    if(event.target.tagName != "LI") return;
+    let li = event.target;
+    let selected = list.querySelectorAll('.selected');
+    for(let elem of selected){
+        elem.classList.remove('selected');
+    }
+    li.classList.add('selected');
+}
